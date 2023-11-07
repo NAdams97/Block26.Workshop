a61703087576c42f39fa220dd42f8b919249e4d0
NAdams97 <nitoad97@gmail.com>
2023-11-07 14:56:59 -0500

    end of class commit

--- Contact-List/src/App.jsx
+++ Contact-List/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import './components/ContactList.jsx'
 
 function App() {
 
   return (
     <>
-      
+      <ContactList />
     </>
   )
 }
